a63924b3b6e3734b81602b44b8bb52697c8df284
bzolnierek <bartosz.zolnierek@rst.com.pl>
2016-04-09 11:40:36 +0200

    front dostał socket

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/app'));
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/app/index.html');
 });
 
 io.on('connection', function(socket){
