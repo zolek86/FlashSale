7a0a5a046a6ffabfaa2b997510e2e18e85f3975e
gotxx <konrad.linek@gmail.com>
2016-04-09 18:06:55 +0200

    location

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,12 +1,39 @@
-    var socket = io('http://localhost:3000');
-    socket.on('gis', function (data) {
-        console.log(data);
-    });
-    $('#findLocation').click(function () {
-        var location = {
-            city: $("#inputMiasto").val(),
-            street: $("#inputUlica").val(),
-            streetNumber: $("#inputNumer").val()
-        };
-        socket.emit('sendGis', location);
-    });
+var flashSale = {
+	socket: io('http://localhost:3000'),
+	init: function(){
+		this.appEvents();
+	},
+	appEvents: function(){
+		var _flashSale = this;
+		this.socket.on('gis', function (data) {
+	        console.log(data);
+	        /*appLocationMsg(data);*/
+	    });
+	    $('#findLocation').click(function () {
+	        var location = {
+	            city: $("#inputMiasto").val(),
+	            street: $("#inputUlica").val(),
+	            streetNumber: $("#inputNumer").val()
+	        };
+	        _flashSale.socket.emit('sendGis', location);
+	    });
+	},
+	appLocationMsg: function(locationData){
+		
+		var errorMsg = "!!! OMG !!! ERROR !!!",
+			msgSpot = $('#locationMsg');
+		msgSpot.html('');
+
+		if(locationData.error === false){
+			msgSpot.append(this.appLocationTemplate(locationData.gis));
+		} else {
+			msgSpot.append(msgSpot);
+		}
+	},
+	appLocationTemplate: function(lat,lang,location){
+		return template = "Twoje położenie to: " + lat + " szerokości " + lang + " długości geograficznej \n" + " adres: " + location;
+	};
+};
+$(function(){
+	flashSale.init();
+});
