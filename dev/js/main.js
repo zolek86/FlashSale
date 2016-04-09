ae39b568a5c573bc3101ee55699afe4602b5edc3
gotxx <konrad.linek@gmail.com>
2016-04-09 17:38:59 +0200

    poprawki do apki

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,12 +1,12 @@
-// var socket = io('http://localhost:3000');
-// socket.on('gis', function (data) {
-//     console.log(data.gis);
-// });
-// $('#findLocation').click(function () {
-//     var location = {
-//         city: $("#inputMiasto").val(),
-//         street: $("#inputUlica").val(),
-//         streetNumber: $("#inputNumer").val()
-//     };
-//     socket.emit('sendGis', location);
-// });
+    var socket = io('http://localhost:3000');
+    socket.on('gis', function (data) {
+        console.log(data);
+    });
+    $('#findLocation').click(function () {
+        var location = {
+            city: $("#inputMiasto").val(),
+            street: $("#inputUlica").val(),
+            streetNumber: $("#inputNumer").val()
+        };
+        socket.emit('sendGis', location);
+    });
